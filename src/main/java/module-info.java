d9ef98092f5e2aaf444d0d83dd6d98cc83315e01
gabrielosorioz <gabrielosorio013@gmail.com>
2024-05-18 10:39:59 -0300

    feat(module): define module structure and dependencies
    Add module-info.java to define the module structure for br.com.idealizeall.universitymanagement.
    Specify dependencies on JavaFX and other libraries, export necessary packages, and open packages for reflection.
    
    - Requires javafx.controls, javafx.fxml, org.controlsfx.controls, com.dlsc.formsfx, and org.kordamp.bootstrapfx.core
    - Opens br.com.idealizeall.universitymanagement and br.com.idealizeall.universitymanagement.controller to javafx.fxml
    - Exports br.com.idealizeall.universitymanagement and br.com.idealizeall.universitymanagement.controller
    
    This setup helps in managing dependencies and encapsulating code effectively.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module br.com.idealizeall.universitymanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens br.com.idealizeall.universitymanagement to javafx.fxml;
+    exports br.com.idealizeall.universitymanagement;
+    exports br.com.idealizeall.universitymanagement.controller;
+    opens br.com.idealizeall.universitymanagement.controller to javafx.fxml;
+}
