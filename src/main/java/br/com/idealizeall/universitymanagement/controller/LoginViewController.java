a2c1e39049630e78e8e8b7bcd9bbc73d7d13ac90
gabrielosorioz <gabrielosorio013@gmail.com>
2024-05-21 19:52:20 -0300

    :construction: feat(Login): adds the control methods for loginForm UI

--- src/main/java/br/com/idealizeall/universitymanagement/controller/LoginViewController.java
+++ src/main/java/br/com/idealizeall/universitymanagement/controller/LoginViewController.java
@@ -1,19 +1,25 @@
 package br.com.idealizeall.universitymanagement.controller;
 
+import br.com.idealizeall.universitymanagement.exception.UserException;
+import br.com.idealizeall.universitymanagement.model.User;
 import br.com.idealizeall.universitymanagement.model.UserRoles;
+import br.com.idealizeall.universitymanagement.model.UserValidation;
+import br.com.idealizeall.universitymanagement.service.UserService;
+import javafx.animation.Animation;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +34,7 @@ public class LoginViewController implements Initializable {
     private AnchorPane adminForm;
 
     @FXML
-    private TextField adminPassword;
+    private PasswordField adminPassword;
 
     @FXML
     private Hyperlink adminSignInBtn;
@@ -49,10 +55,13 @@ public class LoginViewController implements Initializable {
     private PasswordField loginPassword;
 
     @FXML
-    private ComboBox<?> loginRole;
+    private ComboBox<UserRoles> loginRole;
 
     @FXML
     private TextField loginUsername;
+
+    @FXML
+    private Label adminUsernameErrorMsg;
 
     @FXML
     private PasswordField studentConfirmPassword;
@@ -96,9 +105,73 @@ public class LoginViewController implements Initializable {
     @FXML
     private TextField teacherUsername;
 
+    @FXML
+    private Label admPassHasNum;
+
+    @FXML
+    private Label admPassHasLow;
+
+    @FXML
+    private Label admPassHasCap;
+
+    @FXML
+    private Label admPass8Char;
+
+    private UserService userService;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        loginForm();
+
+    }
+
+    public void loginForm(){
+        loginForm.setVisible(true);
+        adminForm.setVisible(false);
+        studentForm.setVisible(false);
+        teacherForm.setVisible(false);
         loadRoles();
+    }
+
+    public void adminForm(){
+        adminForm.setVisible(true);
+        loginForm.setVisible(false);
+        studentForm.setVisible(false);
+        teacherForm.setVisible(false);
+        checkPassword(adminPassword);
+        checkUsername(adminUsername);
+    }
+
+    public void studentForm(){
+        studentForm.setVisible(true);
+        adminForm.setVisible(false);
+        loginForm.setVisible(false);
+        teacherForm.setVisible(false);
+    }
+
+    public void teacherForm(){
+        teacherForm.setVisible(true);
+        studentForm.setVisible(false);
+        adminForm.setVisible(false);
+        loginForm.setVisible(false);
+
+    }
+
+    public void switchForm(ActionEvent actionEvent){
+         UserRoles selectedItem = loginRole.getSelectionModel().getSelectedItem();
+         switch(selectedItem){
+             case ADMIN:
+                 adminForm();
+                 break;
+             case STUDENT:
+                 studentForm();
+                 break;
+             case TEACHER:
+                 teacherForm();
+                 break;
+             default:
+                 break;
+         }
 
     }
 
@@ -108,8 +181,133 @@ public class LoginViewController implements Initializable {
         loginRole.setItems(dataList);
     }
 
+    void checkPassword(PasswordField passwordField){
+        passwordField.textProperty().addListener(new ChangeListener<String>() {
 
+            boolean hasNum = false;
+            boolean hasCap = false;
+            boolean hasLow = false;
+            boolean hasLenght = false;
+
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                hasCap = newValue.matches(".*[A-Z].*");
+                hasLow  = newValue.matches(".*[a-z].*");
+                hasNum = newValue.matches(".*\\d.*");
+                hasLenght = newValue.length() > 7;
+
+                if (hasLenght){
+                    admPass8Char.setStyle("-fx-text-fill: #3dcc00;");
+                } else {
+                    admPass8Char.setStyle("-fx-text-fill: #FFFFFF;");
+                }
+                if(hasCap){
+                    System.out.println("Contém maiúscula");
+                    admPassHasCap.setStyle("-fx-text-fill: #3dcc00;");
+
+                } else {
+                    System.out.println("Não contém maiúscula");
+                    admPassHasCap.setStyle("-fx-text-fill: #FFFFFF;");
+                }
+                if(hasNum){
+                    admPassHasNum.setStyle("-fx-text-fill: #3dcc00;");
+                    System.out.println("Contém Número");
+                } else {
+                    admPassHasNum.setStyle("-fx-text-fill: #FFFFFF;");
+                    System.out.println("Não contém número");
+                }
+                if(hasLow){
+                    System.out.println("Contém letra minúscula");
+                    admPassHasLow.setStyle("-fx-text-fill: #3dcc00;");
+                } else {
+                    admPassHasLow.setStyle("-fx-text-fill: #FFFFFF;");
+                    System.out.println("Não contém letra minúscula");
+                }
+                if (hasLow && hasLenght && hasCap && hasNum){
+                    adminPassword.setStyle("-fx-border-color: #3dcc00;");
+                } else {
+                    adminPassword.setStyle("-fx-border-color: #d1d1d1;");
+                }
+            }
+        });
+    }
+
+    //Task , validar se os campos password e confirmPassword são iguais.
+
+    void checkUsername(TextField username){
+        username.textProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                System.out.println("Alterado.");
+                username.setStyle("-fx-border-color: #ffff");
+                adminUsernameErrorMsg.setVisible(false);
+            }
+        });
+    }
+
+    public void saveUser(ActionEvent actionEvent){
+        if(fieldIsNotBlank(adminPassword,adminUsername,adminConfirmPassword)){
+
+
+        } else {
+            userService = new UserService();
+            User newUser = User.builder()
+                    .username(adminUsername.getText())
+                    .password(adminPassword.getText())
+                    .dataCreate(LocalDateTime.now())
+                    .role(UserRoles.ADMIN)
+                    .build();
+            try {
+
+                userService.registerUser(newUser);
+            } catch (UserException e) {
+                handleUserException(e);
+            }
+            userService = null;
+        }
+    }
+
+    public void showErrorUsername(TextField username){
+        username.setStyle("-fx-border-color: #fc2f2f;");
+        checkUsername(username);
+    }
+
+    public void showErrorPassword(PasswordField passwordField){
+        passwordField.setStyle("-fx-border-color: #fc2f2f;");
+
+    }
+
+    void handleUserException(UserException exception){
+        String exceptionMessage = exception.getMessage();
+        switch (exceptionMessage){
+            case "Invalid user password":
+                showErrorPassword(adminPassword);
+                break;
+            case "User already exists":
+                showErrorUsername(adminUsername);
+            case "Username is blank":
+
+            default:
+                break;
+        }
+
+    }
+
+    boolean fieldIsNotBlank(PasswordField password, TextField username, PasswordField confirmPassword){
+        boolean isValid = false;
+
+        boolean hasPass = !password.getText().isBlank();
+        boolean hasUser = !username.getText().isBlank();
+        boolean hasCPass = !confirmPassword.getText().isBlank();
+
+        if(hasCPass && hasUser && hasPass){
+            isValid = true;
+        }
+
+        return isValid;
+    }
     public void handleBtnClick(ActionEvent actionEvent) {
-        System.out.println("Hello world !");
+
+
     }
 }
