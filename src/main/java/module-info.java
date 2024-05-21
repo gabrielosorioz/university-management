26c7105dbc96f1b9f8aed5b8ee0d7842219a394b
gabrielosorioz <gabrielosorio013@gmail.com>
2024-05-21 19:47:36 -0300

    :green_heart: build: update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module br.com.idealizeall.universitymanagement {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+    requires jdk.compiler;
 
     opens br.com.idealizeall.universitymanagement to javafx.fxml;
     exports br.com.idealizeall.universitymanagement;
