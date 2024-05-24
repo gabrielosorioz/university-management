5f3121b555123486946fa2811b19aac185b93678
gabrielosorioz <gabrielosorio013@gmail.com>
2024-05-24 10:29:20 -0300

    feat(user services): introduces method to supports build user object
    modified: UserService.java

--- src/main/java/br/com/idealizeall/universitymanagement/service/UserService.java
+++ src/main/java/br/com/idealizeall/universitymanagement/service/UserService.java
@@ -2,9 +2,11 @@ package br.com.idealizeall.universitymanagement.service;
 
 import br.com.idealizeall.universitymanagement.exception.UserException;
 import br.com.idealizeall.universitymanagement.model.User;
+import br.com.idealizeall.universitymanagement.model.UserRoles;
 import br.com.idealizeall.universitymanagement.model.UserValidation;
 import br.com.idealizeall.universitymanagement.repository.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.logging.Logger;
 
 public class UserService {
@@ -12,21 +14,32 @@ public class UserService {
 
     UserRepository userRepository = new UserRepository();
 
+    public User createUserByRole(UserRoles role, String username, String password, String email){
+        return User.builder()
+                .username(username)
+                .email(email)
+                .password(password)
+                .dataCreate(LocalDateTime.now())
+                .role(role)
+                .build();
+    }
+
     public void registerUser(User user) throws UserException {
        if(!userRepository.existsByUsername(user.getUsername())){
            if(UserValidation.validatePassword(user.getPassword())){
                if(UserValidation.validateUsername(user.getUsername())){
-                    user.getUsername().toLowerCase();
+
                } else {
                    throw new UserException("Username is blank");
                }
            } else {
-               throw new UserException("Invalid user password");
+               throw new UserException("Invalid user password" + user.getPassword());
            }
        } else {
-           throw new UserException("User already exists");
+           throw new UserException("Username already exists");
        }
-       userRepository.save(user);
+        userRepository.save(user);
+
     }
 
 
