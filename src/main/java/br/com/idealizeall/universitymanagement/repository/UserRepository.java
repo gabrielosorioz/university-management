f170acae03022de04706cc1ed616f2439fe5cf6b
gabrielosorioz <gabrielosorio013@gmail.com>
2024-06-07 16:03:36 -0300

    :recycle: refactor `UserRepository` class.
    - Remove unnecessary codes
    - Introduce Inversion Of Control dependecy
    - Check if username is blank
    
    Modified: UserRepository.java

--- src/main/java/br/com/idealizeall/universitymanagement/repository/UserRepository.java
+++ src/main/java/br/com/idealizeall/universitymanagement/repository/UserRepository.java
@@ -11,29 +11,12 @@ public class UserRepository {
 
     private Connection connection;
 
-    protected void connect(){
-        try {
-            if(connection == null || connection.isClosed()){
-                connection = DBConfig.getPSQLConnection();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    protected void disconnect(){
-        try {
-            if(connection != null && !connection.isClosed()){
-                connection.close();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public UserRepository(Connection connection){
+        this.connection = connection;
     }
 
     public void save(User user) {
         String sql = "INSERT INTO users (username,email,password,data_create,role) VALUES (?,?,?,?,?)";
-        connect();
         try (PreparedStatement pstmt = connection.prepareStatement(sql)){
             pstmt.setString(1, user.getUsername());
             pstmt.setString(2,user.getEmail());
@@ -41,7 +24,6 @@ public class UserRepository {
             pstmt.setTimestamp(4, Timestamp.valueOf(user.getDataCreate()));
             pstmt.setString(5, user.getRole().name());
             boolean rowAffected = pstmt.executeUpdate() > 0;
-            disconnect();
 
             if(rowAffected){
                 log.info("----------------------------User inserted successfully----------------------------");
@@ -51,50 +33,22 @@ public class UserRepository {
 
         } catch (SQLException ex) {
             log.severe("Error at insert user " + ex.getLocalizedMessage() + "SQL State: " + ex.getSQLState());
-        } finally {
-            disconnect();
         }
     }
 
     public boolean existsByUsername(String username){
+        if (username.isBlank() || username.equals("")){
+            return false;
+        }
         String sql = "SELECT * FROM users WHERE username = ?";
-        connect();
         try (PreparedStatement pstmt = connection.prepareStatement(sql)){
             pstmt.setString(1, username);
             ResultSet rs = pstmt.executeQuery();
-
             return rs.next();
         } catch (SQLException ex){
             ex.getLocalizedMessage();
             ex.printStackTrace();
-        } finally {
-            disconnect();
         }
         return false;
-    }
-
-    public void update(User user){
-        String sql = "INSERT INTO users (username,email,password,data_update,role) VALUES (?,?,?,?,?)";
-        connect();
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)){
-            pstmt.setString(1,user.getUsername());
-            pstmt.setString(2, user.getEmail());
-            pstmt.setString(3, user.getPassword());
-            pstmt.setTimestamp(4,Timestamp.valueOf(user.getDataUpdate()));
-            pstmt.setString(5,user.getRole().name());
-            boolean rowAffected = pstmt.executeUpdate() > 0;
-            disconnect();
-
-            if(rowAffected){
-                log.info("User updated successfully");
-            } else {
-                log.severe("Error at update User");
-            }
-
-        } catch (SQLException ex){
-            log.severe("Couldn't update user");
-            ex.printStackTrace();
-            disconnect();
-        }
     }
 }
